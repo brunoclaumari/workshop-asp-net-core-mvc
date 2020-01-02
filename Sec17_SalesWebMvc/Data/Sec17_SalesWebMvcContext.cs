c04f442f99dfc4c6bc78b99ea6f6fc6aa553f1fb
brunoclaumari <brunoclaumari@gmail.com>
2020-01-02 15:59:24 -0300

    CRUD Scaffolding

---
+++ Sec17_SalesWebMvc/Data/Sec17_SalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sec17_SalesWebMvc.Models
+{
+    public class Sec17_SalesWebMvcContext : DbContext
+    {
+        public Sec17_SalesWebMvcContext (DbContextOptions<Sec17_SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sec17_SalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- Sec17_SalesWebMvc/Startup.cs
+++ Sec17_SalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Sec17_SalesWebMvc.Models;
 
 namespace Sec17_SalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace Sec17_SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Sec17_SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Sec17_SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
